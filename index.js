09f8ba8b2577b338bb1fa73b268c7532c77049c9
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-16 09:00:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function Scooter(year,color,model){
+  return(
+      this.year = year,
+      this.color = color,
+      this.model = model
+    )
+}
+
+function Driver(name,age,experience){
+  return(
+      this.name = name,
+      this.age = age,
+      this.experience = experience
+    )
+}
+
+function PickupLocation(address,city){
+  return(
+      this.address = address,
+      this.city = city
+    )
+}
